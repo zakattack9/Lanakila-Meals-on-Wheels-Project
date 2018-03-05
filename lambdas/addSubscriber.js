0c82b634a8e428337b85d7f86cb1d19a5997a643
Yoscroll <ianhenry.c808@gmail.com>
2018-03-04 20:08:26 -1000

    Update addSubscriber.js

--- lambdas/addSubscriber.js
+++ lambdas/addSubscriber.js
@@ -4,8 +4,8 @@ var sns = new AWS.SNS();
 
 var params = {
   Protocol: 'sms',
-  TopicArn: 'arn:aws:sns:us-west-2:116598778905:phone_numbers', 
-  Endpoint: '+18085417092'
+  TopicArn: '', 
+  Endpoint: ''
 };
 sns.subscribe(params, function(err, data) {
   if (err) console.log(err, err.stack); // an error occurred
