b370c79f3f9ee104ca1f013febc2512f1c4dbb8b
Yoscroll <ianhenry.c808@gmail.com>
2018-03-06 22:12:15 -1000

    finished createTopic lambda

--- lambdas/createTopic.js
+++ lambdas/createTopic.js
@@ -5,6 +5,14 @@ AWS.config.region = 'us-west-2';
 var sns = new AWS.SNS();
 
 module.exports.createTopic = (event, context, callback) => {
+	let msgID; //generated ID
+	( function () { msgID = Math.random().toString(36).substr(2, 5) }() ); //self-invoking function generates ID
+	console.log(msgID);
+
+	// Create the DynamoDB item
+	const dynamoDB = new AWS.DynamoDB.DocumentClient({apiVersion: '2012-10-08'});
+
+
 	const htmlParams = JSON.parse(event.body);
 	var params = {
 		Name: htmlParams.name
@@ -25,6 +33,23 @@ module.exports.createTopic = (event, context, callback) => {
 			callback(null, response);
 		}
 		else{
+			let params = {
+				  TableName: 'TopicList',
+				  Item: {
+				  	'id' : msgID,
+				    'arn' : data.TopicArn,
+				    'name' : htmlParams.name
+				  }
+				};
+
+				// Call DynamoDB to add the item to the table
+				dynamoDB.put(params, function(dberr, dbdata) {
+				  if (err) {
+				    console.log("Error", dberr);
+				  } else {
+				    console.log("Success", dbdata);
+				  }
+				});
 			const response = {
 				statusCode: 200,
 				headers: {
@@ -40,3 +65,18 @@ module.exports.createTopic = (event, context, callback) => {
 	    }
 	})
 };
+/*
+var AWS = require('aws-sdk');
+AWS.config.region = 'us-west-2';
+var sns = new AWS.SNS();
+var params = {
+		Name: "testingf"
+	};
+sns.createTopic(params, function(err, data) {
+		if (err){ 
+			console.log(err)
+		}
+		else{
+			console.log(data.TopicArn)
+		}
+	});*/
